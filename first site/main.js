4827c251c00174ebfff2758c21e46075e806c8b7
NefdovaM <4803328@gmail.com>
2023-10-05 19:33:55 +0300

    my first website

---
+++ first site/main.js
@@ -0,0 +1,47 @@
+/*
+let a = 152;
+let b =2;
+let sum = a + b;
+let mult = a * b;
+let div = a / b;
+console.log(sum, mult, div)
+*/
+
+/*
+let name = prompt("Your name?");
+console.log(`Hi, ${name}!`);
+*/
+
+/*
+function printName(name){
+console.log(`Hi, ${name}!`);
+};
+printName();
+*/
+
+/*
+let result = confirm ("Are you sure?");
+if(result){
+	console.log("We are happy, that you are sure!");
+} else {
+	console.log("It`s a pity, that you are not sure!");
+}
+*/
+
+
+
+let age = 30;
+switch(age) {
+	case 18:
+		console.log("You are an adult, anything is possible!");
+		break;
+	case 10:
+		console.log("You need to learn lessons!");
+		break;
+	case 30:
+		console.log("Go to sleep, tomorrow you need to go to the work!");
+		break;
+		default:
+		console.log("We are don`t know what you must do");
+}
+
